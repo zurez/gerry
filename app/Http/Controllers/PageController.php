6f61214eea5c6694994ab7ee8f048bde6004897c
zurez <zurez4u@gmail.com>
2017-08-07 20:18:42 +0530

    ff

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -151,13 +151,15 @@ class PageController extends Controller
             $n->message=$r->message;
             $n->save();
             $femail=DB::table('global')->pluck('forwarding_email');
+            dump($femail);
             $e=new EmailController;
-            $e->forward_email($r,$femail);
+
+            return $e->forward_email($r,$femail);
             Session::flash('flash_message','Your message has been sent. Thank you for contacting');
         } catch (\Exception $e) {
-            
+            dump($e);
         }
         
-        return redirect()->back();
+        // return redirect()->back();
     }
 }

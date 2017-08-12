94d95f9cf924bca614d2c5cd657abc7b3501ed76
zurez <zurez4u@gmail.com>
2017-08-12 11:52:43 +0530

    ff

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,35 @@
+@extends('layout.template')
+@section('content')
+ 
+  {{--   <div class="page-title title-size-normal text-dark">
+      <div class="container">
+        <div class="inner">
+          <div class="column">
+            <h1>404</h1>
+          </div>
+          <div class="column">
+            <div class="breadcrumbs">
+              <!-- Breadcrumb NavXT 5.6.0 -->
+              <span property="itemListElement" typeof="ListItem"><a property="item" typeof="WebPage" title="Go to StartApp." href="http://startapp.8guild.com" class="home"><span property="name">Home</span></a>
+              <meta property="position" content="1">
+              </span> <i class="material-icons keyboard_arrow_right"></i> <span property="itemListElement" typeof="ListItem"><a property="item" typeof="WebPage" title="Go to Blog List No Sidebar." href="http://startapp.8guild.com/blog-list-no-sidebar/" class="post-root post post-post"><span property="name">Blog List No Sidebar</span></a>
+              <meta property="position" content="2">
+              </span> <i class="material-icons keyboard_arrow_right"></i> <span property="itemListElement" typeof="ListItem"><span property="name">404</span>
+              <meta property="position" content="3">
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div> --}}
+
+    <div class="container text-center padding-bottom-8x">
+      <h3>Oops! That page can&rsquo;t be found.</h3>
+      <p class="padding-bottom-1x">It looks like nothing was found at this location.</p>
+   
+    </div>
+
+
+
+   
+@stop

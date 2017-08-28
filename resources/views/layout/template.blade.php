5d4a50e7e3c87b604b225dfd3113deb43686a7bb
zurez <zurez4u@gmail.com>
2017-08-28 17:40:15 +0530

    gg

--- resources/views/layout/template.blade.php
+++ resources/views/layout/template.blade.php
@@ -768,6 +768,20 @@ Student hotel software
 			<!-- *************************************************************************************** -->
 		<script type="text/javascript" src="{{asset('assets/js/startapp-theme.js')}}"></script>
 		<!-- **************************************************************************************************************************** -->
+		{{-- TAWK --}}
+		<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/59a40716b6e907673de0a0be/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
 
 
 </body>
